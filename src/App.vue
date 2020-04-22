1c9effe3cc09d2e1c462075429b80df9a9ca7fd5
Gideon Maina <maina.gidi@gmail.com>
2020-04-22 18:35:39 +0300

    task: Working Todos app with initial test data

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-on:del-todo="deleteTodoItem" v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Todos, Header, AddTodo },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'My Love',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Mi A3',
+          completed: false,
+        },
+        { id: 3, title: 'Mothers day', completed: false },
+        {
+          id: 4,
+          title: 'FIA Formula One Race',
+          completed: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    deleteTodoItem(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos.push(newTodo);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
 }
 </style>
